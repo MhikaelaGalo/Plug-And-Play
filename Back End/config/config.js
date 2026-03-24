bc2569d93953ebb0966c765c41c647cc8f826b77
MhikaelaGalo <mhikaela.galo@ciit.edu.ph>
2026-03-24 12:49:37 +0800

    fix config bug

--- Back End/config/config.js
+++ Back End/config/config.js
@@ -15,7 +15,7 @@ export async function connectToDb() {
   db = client.db(dbName);
 
   console.log("✅ Connected to MongoDB");
-  console.log("Using DB:", PlugAndPlay);
+  console.log("Using DB:", dbName);
 
   return db;
 }
